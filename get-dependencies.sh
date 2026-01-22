56f30c6f7e43acc164dc2ba988bbcdde7ad43bca
Link4Electronics <link4electronics@gmail.com>
2026-01-22 05:45:42 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,7 +17,6 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package soh-otr-exporter
 make-aur-package
 
 # If the application needs to be manually built that has to be done down here
