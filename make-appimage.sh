41373d3604e674d2fbffea76c6e76f72d9cb5ef9
Link4Electronics <link4electronics@gmail.com>
2026-02-08 13:46:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,19 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q soh | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/soh.png
-export DESKTOP=/usr/share/applications/soh.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/soh /usr/bin/soh-otr-exporter /usr/bin/zenity
-mv /opt/soh/soh.o2r ./AppDir/bin
-mv /opt/soh/gamecontrollerdb.txt ./AppDir/bin
+#mv /opt/soh/soh.o2r ./AppDir/bin
+#mv /opt/soh/gamecontrollerdb.txt ./AppDir/bin
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
