46d58d0143eb901776956f19afa6ff2a001a1b90
Link4Electronics <link4electronics@gmail.com>
2026-02-19 02:47:49 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,8 @@ export DESKTOP=/usr/share/applications/soh.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/soh /usr/bin/soh-otr-exporter /usr/bin/zenity
+#quick-sharun /usr/bin/soh /usr/bin/soh-otr-exporter /usr/bin/zenity
+quick-sharun /usr/bin/soh /usr/bin/zenity
 mv /opt/soh/soh.o2r ./AppDir/bin
 mv /opt/soh/gamecontrollerdb.txt ./AppDir/bin
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
